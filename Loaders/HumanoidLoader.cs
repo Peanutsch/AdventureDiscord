c05174316dffc95cce55269f17d26430d80f53c8
UniqueDanique <DaniqueDikken@proton.me>
2025-10-14 12:02:04 +0200

    Catagorized humanoids.json
    * New HumanoidModel
    * Updated HumanoidLoader to new catagorized humanoids.json

--- Loaders/HumanoidLoader.cs
+++ Loaders/HumanoidLoader.cs
@@ -4,12 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Adventure.Models.NPC;
+using Adventure.Services;
 
 namespace Adventure.Loaders
 {
     public static class HumanoidLoader
     {
-        public static List<NpcModel>? Load() =>
-            JsonDataManager.LoadListFromJson<NpcModel>("Data/NPC/humanoids.json");
+        public static List<NpcModel>? Load()
+        {
+            try
+            {
+                var humanoids = JsonDataManager.LoadObjectFromJson<HumanoidModel>("Data/NPC/humanoids.json");
+
+                if (humanoids == null)
+                {
+                    LogService.Error("[HumanoidLoader] > Failed to load humanoids.json.\n");
+                    return null;
+                }
+
+                // Combine all categories into a single list
+                var allNpcs = new List<NpcModel>();
+                if (humanoids.Humanoids != null)
+                {
+                    LogService.Info($"Adding catagory Humanoids to allNpcs: {humanoids.Humanoids.Count} Humanoids");
+                    allNpcs.AddRange(humanoids.Humanoids);
+                }
+
+                if (humanoids.Undead!= null)
+                {
+                    LogService.Info($"Adding catagory Undead to allNpcs: {humanoids.Undead.Count} Undead");
+                    allNpcs.AddRange(humanoids.Undead);
+                }
+
+                if (humanoids.Human!= null)
+                {
+                    LogService.Info($"Adding catagory Humanoids to allNpcs: {humanoids.Human.Count} Humans");
+                    allNpcs.AddRange(humanoids.Human);
+                }
+
+                if (humanoids.Elf!= null)
+                {
+                    LogService.Info($"Adding catagory Elf to allNpcs: {humanoids.Elf.Count} Elfs");
+                    allNpcs.AddRange(humanoids.Elf);
+                }
+
+                if (humanoids.Dwarf!= null)
+                {
+                    LogService.Info($"Adding catagory Dwarf to allNpcs: {humanoids.Dwarf.Count} Dwarfs");
+                    allNpcs.AddRange(humanoids.Dwarf);
+                }
+
+                LogService.Info($"[HumanoidLoader] > Loaded total of {allNpcs.Count} NPCs from humanoids.json\n");
+
+                return allNpcs;
+            }
+            catch (System.Exception ex)
+            {
+                LogService.Error($"[HumanoidLoader] > Error loading humanoids: {ex.Message}\n");
+                return null;
+            }
+        }            
     }
 }
