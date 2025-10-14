08c2a0c8373cdbd276edb0380bd431198f13f47e
UniqueDanique <DaniqueDikken@proton.me>
2025-10-14 11:29:21 +0200

    Try/Catch
    * Added Try/Catch in loaders

--- Loaders/WeaponLoader.cs
+++ Loaders/WeaponLoader.cs
@@ -5,44 +5,52 @@ namespace Adventure.Loaders
 {
     public static class WeaponLoader
     {
-        public static List<WeaponModel> Load()
+        public static List<WeaponModel>? Load()
         {
-            var combinedWeapons = new List<WeaponModel>();
-
-            var arrows = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/arrows.json");
-            if (arrows != null)
+            try
             {
-                LogService.Info($"Adding catagory Arrows to combinedWeapons: {arrows.Count} of arrows");
-                combinedWeapons.AddRange(arrows);
-            }
+                var combinedWeapons = new List<WeaponModel>();
 
-            var melee = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/melee.json");
-            if (melee != null)
+                var arrows = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/arrows.json");
+                if (arrows != null)
+                {
+                    LogService.Info($"Adding catagory Arrows to combinedWeapons: {arrows.Count} of arrows");
+                    combinedWeapons.AddRange(arrows);
+                }
+
+                var melee = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/melee.json");
+                if (melee != null)
+                {
+                    LogService.Info($"Adding catagory Melee to combinedWeapons: {melee.Count} of melee weapons");
+                    combinedWeapons.AddRange(melee);
+                }
+
+
+                var range = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/range.json");
+                if (range != null)
+                {
+                    LogService.Info($"Adding catagory Range to combinedWeapons: {range.Count} of ranged weapons");
+                    combinedWeapons.AddRange(range);
+                }
+
+                /*
+                var throwWeapons = JsonDataLoader.LoadListFromJson<WeaponModel>("Data/Items/Weapons/throw.json");
+                if (throwWeapons != null)
+                {
+                    LogService.Info($"Adding catagory Trow Weapons to combinedWeapons: {trowWeapons.Count} of trown weapons");
+                    combinedWeapons.AddRange(throwWeapons);
+                }
+                */
+
+                LogService.Info($"Loaded total of {combinedWeapons.Count} weapons\n");
+
+                return combinedWeapons;
+            }
+            catch (System.Exception ex)
             {
-                LogService.Info($"Adding catagory Melee to combinedWeapons: {melee.Count} of melee weapons");
-                combinedWeapons.AddRange(melee);
+                LogService.Error($"[WeaponLoader] > Error loading weapons: {ex.Message}\n");
+                return null;
             }
-                
-
-            var range = JsonDataManager.LoadListFromJson<WeaponModel>("Data/Items/Weapons/range.json");
-            if (range != null)
-            {
-                LogService.Info($"Adding catagory Range to combinedWeapons: {range.Count} of ranged weapons");
-                combinedWeapons.AddRange(range);
-            }
-
-            /*
-            var throwWeapons = JsonDataLoader.LoadListFromJson<WeaponModel>("Data/Items/Weapons/throw.json");
-            if (throwWeapons != null)
-            {
-                LogService.Info($"Adding catagory Trow Weapons to combinedWeapons: {trowWeapons.Count} of trown weapons");
-                combinedWeapons.AddRange(throwWeapons);
-            }
-            */
-
-            LogService.Info($"Loaded total of {combinedWeapons.Count} weapons\n");
-
-            return combinedWeapons;
         }
 
     }
